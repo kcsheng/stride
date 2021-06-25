362d389926a2979702275e56617b68d221df581e
kcsheng <kacysheng@mail.com>
2021-06-26 08:25:29 +1000

    FComplete initial color scheme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 const dayElement = $("#currentDay");
 const timeElement = $("#currentTime");
 const containerElement = document.querySelector(".container")
-let currentHour;
+let currentHourIn24;
+let allHourElements;
 
 function getCurrentTime() {
   let currentDay = moment().format("dddd, MMMM Do");
   let currentTime = moment().format("h:mm:ss a");
-  currentHour = moment().format("h")
   dayElement.text(currentDay);
   timeElement.text(currentTime);
 }
@@ -23,37 +23,51 @@ function populateForm() {
       newRow = `
         <div class="form-row">
           <div class="col-2 hour" id="time" data-hour="${i}">${i}</div>
-          <div class="col-8" id="textarea"><textarea class="form-control border-0" id="tasks" name="tasks"></textarea></div>
+          <div class="col-8" id="textarea"><textarea class="form-control text-white border-0" id="tasks" name="tasks"></textarea></div>
           <div class="col-2" id="btn"><button class="saveBtn" id="save-btn"><i class="fas fa-save fa-2x"></i></button></div>
         </div>`
       result += newRow;
     } 
   containerElement.innerHTML = result;
-  // Ajust the hours to 12 hours format
-  const allHours = document.querySelectorAll("#time");
-  for(let j = 0; j < allHours.length; j++) {
-    let nthHour = allHours[j];
+  // Ajust the hours to 12 hour format
+  allHourElements = document.querySelectorAll("#time");
+  for(let j = 0; j < allHourElements.length; j++) {
+    let nthHour = allHourElements[j];
     if(nthHour.innerText < 12) {
       nthHour.innerText += "AM";
     } else if(nthHour.innerText == 12) {
       nthHour.innerText += "PM";
     } else {
-      nthHour.innerText = nthHour.innerText - "12" + "PM"
+      nthHour.innerText = nthHour.innerText - "12" + "PM";
     }
   }
 }
 
-// function coloriseTimeBlocks() {
-//   setInterval(() => {
+function coloriseTimeBlocks() {
+  console.log(allHourElements)
+  setInterval(() => {
+    allHourElements.forEach((element) => {
+      let hour = element.dataset.hour;
+      currentHourIn24 = moment().format("hh");
+      let nextElement = element.nextElementSibling
+      if(hour > currentHourIn24) {
+        nextElement.classList.add("future");
+      } else if(hour == currentHour) {
+        nextElement.classList.add("present");
+      } else {
+        nextElement.classList.add("past");
+      }
+    });
 
-//   }, 1000);
+    
+  },1000);
   
-// }
+}
 
 function init() {
   showLiveTime();
   populateForm();
-  // coloriseTimeBlocks();
+  coloriseTimeBlocks();
 }
 
 init();
