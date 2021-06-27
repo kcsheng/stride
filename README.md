c6e2534af836fdf25b7c9a42b9933ca8d87c8295
kcsheng <kacysheng@mail.com>
2021-06-27 10:39:24 +1000

    Add access link and minor wording/space adjustments

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
-Stride is a day planner that allows a user to save daily tasks on an hourly basis. Click here to access the planner.
+Stride is a day planner that allows a user to save daily tasks on an hourly basis. Click [here](https://kcsheng.github.io/stride/) to access the planner.
 
 ### Feature
-____
+
+---
+
 - Current day and time display
 - Color coded time-blocks (past, present and future)
 - Memorised events for the day
 - Task update
-- Fullly responsive, utilizing bootstrap for underlying queries
+- Fully responsive, utilizing bootstrap for underlying queries
 
 ### Wireframe
-____
+
+---
+
 Inital wireframe for the project: <br>
 ![wireframe plan for stride](./assets/img/wireframe.png)
 
 ### DRY
-____
-Utilise javascript machinery to churn out repetitive js and css time-block code to achieve DRY.
 
+---
+
+Utilise javascript machinery to churn out repetitive js and css time-block code to achieve DRY.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
 const dayElement = $("#currentDay");
 const timeElement = $("#currentTime");
-const containerElement = document.querySelector(".container")
+const containerElement = document.querySelector(".container");
 let currentHourIn24;
 let allHourElements;
-
 
 function getCurrentTime() {
   let currentDay = moment().format("dddd, MMMM Do");
@@ -19,23 +18,23 @@ function showLiveTime() {
 // Populate 9 hour time slots, place timepoint in dataset as a hook
 function populateForm() {
   let result = ``;
-  for(let i = 9; i <= 17; i++) {
+  for (let i = 9; i <= 17; i++) {
     let newRow = `
         <div class="form-row">
           <div class="col-2 hour" id="time" data-hour="${i}">${i}</div>
           <div class="col-8" id="textarea"><textarea class="form-control text-white border-0" id="tasks"></textarea></div>
           <div class="col-2" id="btn"><button class="saveBtn" id="save-btn"><i class="fas fa-save fa-2x" id="save-icon"></i></button></div>
-        </div>`
-      result += newRow;
-    } 
+        </div>`;
+    result += newRow;
+  }
   containerElement.innerHTML = result;
   // Ajust the hours to 12 hour format
   allHourElements = document.querySelectorAll("#time");
-  for(let j = 0; j < allHourElements.length; j++) {
+  for (let j = 0; j < allHourElements.length; j++) {
     let nthHour = allHourElements[j];
-    if(nthHour.innerText < 12) {
+    if (nthHour.innerText < 12) {
       nthHour.innerText += "AM";
-    } else if(nthHour.innerText == 12) {
+    } else if (nthHour.innerText == 12) {
       nthHour.innerText += "PM";
     } else {
       nthHour.innerText = nthHour.innerText - "12" + "PM";
@@ -50,16 +49,16 @@ function paintTimeBlocks() {
 function colouriseEachTimeBlock() {
   allHourElements.forEach((element) => {
     let hour = parseInt(element.dataset.hour);
-    currentHourIn24 = parseInt(moment().format("HH"))
-    let nextElement = element.nextElementSibling
-    if(hour < currentHourIn24) {
+    currentHourIn24 = parseInt(moment().format("HH"));
+    let nextElement = element.nextElementSibling;
+    if (hour < currentHourIn24) {
       nextElement.classList.add("past");
-    } else if(hour == currentHourIn24) {
+    } else if (hour == currentHourIn24) {
       nextElement.classList.add("present");
     } else {
       nextElement.classList.add("future");
     }
-  });  
+  });
 }
 
 // Program initiation
@@ -76,29 +75,29 @@ $(containerElement).on("click", "button", storeTasks);
 function storeTasks(e) {
   e.preventDefault();
   let target = $(e.target);
-  // Traverse to find the corresponding time element 
+  // Traverse to find the corresponding time element
   let targetTimeElement = target.closest(".form-row").children("#time");
   let timeAtSave = targetTimeElement.attr("data-hour");
   // Traverse to find the textarea that may contain tasks
   let targetTextArea = targetTimeElement.next().find("textarea#tasks");
   let targetTextAreaValue = targetTextArea.val();
   // Write tasks to local storage
-  if(targetTextAreaValue) {
-      localStorage.setItem(timeAtSave, targetTextAreaValue);
+  if (targetTextAreaValue) {
+    localStorage.setItem(timeAtSave, targetTextAreaValue);
   }
 }
 
 function showTasks() {
-  for(let i = 0; i < localStorage.length; i++) {
+  for (let i = 0; i < localStorage.length; i++) {
     let timePoint = localStorage.key(i);
     let textValue = localStorage.getItem(timePoint);
     allHourElements = document.querySelectorAll("#time");
     allHourElements.forEach((element) => {
-      // Reverse dom traversal 
-      if(element.dataset.hour === timePoint) {
+      // Reverse dom traversal
+      if (element.dataset.hour === timePoint) {
         element.nextElementSibling.firstChild.innerText = textValue;
       }
-    })
+    });
   }
 }
 showTasks();
